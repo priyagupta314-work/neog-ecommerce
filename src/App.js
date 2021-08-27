3694b238eb79f2177c3984ba599b12804d8a7ee7
priyagupta314-work <75786939+priyagupta314-work@users.noreply.github.com>
2021-08-27 23:20:36 +0530

    fixed page url

--- src/App.js
+++ src/App.js
@@ -13,13 +13,13 @@ function App() {
     <div className="App">
            <nav className="nav-bar">
             <ul className="nav-list">
-              <Link to="/" className="nav-pills"><li > Home </li></Link>
+              <Link to="/neog-ecommerce" className="nav-pills"><li > Home </li></Link>
               <Link to="/Wishlist" className="nav-pills"><li > Wishlist </li></Link>
               <Link to="/Cart" className="nav-pills"><li className="nav-list-item"> Cart <span className="badge-round">{itemsInCart.length}</span> </li> </Link>
             </ul>
           </nav>
           <Routes>
-            <Route path='/' element = {<Home />} />
+            <Route path='/neog-ecommerce' element = {<Home />} />
             <Route path='Products' element = {<Products />} />
             <Route path='Cart' element = {<Cart />} />
             {/* <Route path='/wishlist' element = {<Wishlist />} />  */}
